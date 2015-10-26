7e1b13facfffdd91aabc47fa7ef4f9f5e848ac71
CorySpitzer <coryspitzer@gmail.com>
2015-10-26 14:52:31 -0700

    Define DB initialization

--- little_blog.py
+++ little_blog.py
@@ -3,6 +3,7 @@
 import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
     render_template, flash
+from contextlib import closing
 
 # Configure things
 DATABASE = '/tmp/little_blog.db'
@@ -26,6 +27,21 @@ app.config.from_envvar('LITTLE_BLOG_SETTINGS', silent=True)
 # We will need to connect to the DB in the future
 def connect_to_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+# See http://flask.pocoo.org/docs/0.10/tutorial/dbinit/#tutorial-dbinit
+# And initialize it, too
+def init_db():
+    # While keeping a connection opne for the duration of the with block,
+    with closing(connect_to_db()) as db:
+    # (for an explanation of 'with' see
+    # http://stackoverflow.com/questions/3012488/what-is-the-python-with-statement-designed-for )
+        # With the schema file in read mode,
+        with app.open_resource('schema.sql', 'r') as schema_file:
+            # Execute it
+            db.cursor().executescript(schema_file.read())
+        # And save the results to the db
+        db.commit()
+
 
 # run the app if we are executing the code directly
 if __name__ == '__main__':
